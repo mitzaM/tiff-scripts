01eaae13fce09f94c8684898587b57f0f9f3d4b8
Mihai Man <mitaman89@yahoo.com>
2017-05-29 16:08:41 +0300

    refactored getting cinema codes from the xml files

--- tiff-parser-v2.py
+++ tiff-parser-v2.py
@@ -26,7 +26,7 @@ ALLOW_END = datetime(year=2017, month=6, day=30)
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
 DATE_PRETTY = "%A, %d %B - %X"
 CSV_FIELDS = ["Title", "Cinema", "Start Date", "End Date"]
-XML_TAGS = ["ContentTitleText", "AnnotationText",
+XML_TAGS = ["ContentTitleText", "#AnnotationText",
             "ContentKeysNotValidBefore", "ContentKeysNotValidAfter"]
 AVAILABILITY_ERROR_MSG = (
     "Cannot run, out of availability date ({} - {}).\n\n".format(
@@ -74,7 +74,7 @@ def get_date(date_string, xml_tag):
 
 def get_code(annotation_text, xml_tag):
     if not annotation_text:
-        logging.info("{}: Value not found.".format(xml_tag))
+        # logging.info("{}: Value not found.".format(xml_tag))
         return None
 
     combined = "({})".format(")|(".join(list(CINEMAS.keys())))
@@ -82,7 +82,7 @@ def get_code(annotation_text, xml_tag):
     if code_re:
         return CINEMAS.get(code_re.group(0).upper(), code_re.group(0))
     else:
-        logging.info("Couldn't find cinema code in {}.".format(annotation_text))
+        # logging.info("Couldn't find cinema code in {}.".format(annotation_text))
         return None
 
 CINEMAS = get_cinema_codes(CINEMA_CODES_PATH)
@@ -126,22 +126,27 @@ def parse(filename):
 
     result = {key: None for key in CSV_FIELDS}
     tree = ET.ElementTree(file=filename)
-    tags_not_found = [True] * len(XML_TAGS)
+    fields_not_found = [True] * len(CSV_FIELDS)
 
     for e in tree.getroot().iter():
         tag_name = e.tag[e.tag.rindex("}")+1:]
+        if fields_not_found[CSV_FIELDS.index("Cinema")]:
+            code = get_code(e.text, tag_name)
+            if code:
+                result["Cinema"] = code
+                fields_not_found[CSV_FIELDS.index("Cinema")] = False
         try:
             idx = XML_TAGS.index(tag_name)
-            tags_not_found[idx] = False
+            fields_not_found[idx] = False
             result[CSV_FIELDS[idx]] = func[idx](e.text, XML_TAGS[idx])
         except ValueError:
             pass
 
-        if not any(tags_not_found):
+        if not any(fields_not_found):
             break
 
-    for i, e in enumerate(tags_not_found):
-        e and logging.info("{} not found.".format(XML_TAGS[i]))
+    for i, e in enumerate(fields_not_found):
+        e and logging.info("{} not found.".format(CSV_FIELDS[i]))
 
     logging.info("\n")
     return result
